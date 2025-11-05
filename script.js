b0ffb63240c8f52c45ee9ee976766f729999afb1
HARI PRIYA R <haripriyamanjula48@gmail.com>
2025-11-05 14:02:39 +0530

    Add files via upload
    My Portfolio

---
+++ script.js
@@ -0,0 +1,25 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    threshold: 0.3
+};
+
+const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fade => {
+    appearOnScroll.observe(fade);
+});
